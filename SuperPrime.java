bb81a28fb86a0622cb0a732a16c0bbc77de562a3
swarage <sdcoolio@gmail.com>
2013-09-19 12:00:52 -0400

    Update SuperPrime.java

--- SuperPrime.java
+++ SuperPrime.java
@@ -12,10 +12,12 @@ class SuperPrime {
 			return false;
 		if (n % d == 0)
 			return false;
+		/*
 		if (d >= (int)Math.sqrt(n))
 			return true;
 		else
 			return isPrime(n, d+2);
+		*/
 	}
 	
 	static boolean isSuperPrime(int n) {
@@ -25,28 +27,62 @@ class SuperPrime {
 			return isPrime(n,3);
 		} else {
 			String shortnum = strnum.substring(0,strlen-1);
-			boolean checksOut = isPrime(n,3) && isSuperPrime(Integer.parseInt(shortnum));
-			return checksOut;
+			if (isPrime(n,3)) {
+				return isSuperPrime(Integer.parseInt(shortnum));
+			} else {
+				return false;
+			}
 		}
 	}
 	
 	static void getSuperPrimes(int n) {
+	
+		int init = (int)Math.pow(10,n-1);
+		int N = init * 10;
+		
 		boolean exists = false;
 		int counter = 0;
+		
+		boolean[] primeArr = new boolean[N + 1];
+		
+		
+		for (int i = 2; i <= N; i++) {
+			primeArr[i] = true;
+		}
+		
+		for (int i = 2; i*i <= N; i++) {
+			if (primeArr[i]) {
+				for (int j = i; i*j <= N; j++) {
+					primeArr[i*j] = false;
+				}
+			}
+		}
+		
 		long startTime = System.currentTimeMillis();
-		for (int i = (int)Math.pow(10,n-1)+1; i < (int)Math.pow(10,n); i = i + 2) {
+
+		for (int i = init; i <= N; i++) {
+			if (primeArr[i]) {
+				if (isSuperPrime(i)) {
+					counter++;
+					exists = true;
+					System.out.println(i);
+				}	
+			}
+		}
+		
+		long endTime = System.currentTimeMillis();
+		/*for (int i = init; i < init * 10; i += 2) {
 			if (isSuperPrime(i)) {
 				counter++;
 				exists = true;
 				System.out.println(i);
 			}
-		}
+		}*/
 		if (exists) {
 			System.out.println("There are " + counter + " super primes with " + n + " digits");
 		} else {
 			System.out.println("There are no super primes with " + n + " digits");
 		}
-		long endTime = System.currentTimeMillis();
 		System.out.println("It took " + (endTime - startTime) + " milliseconds");
 	}
 
